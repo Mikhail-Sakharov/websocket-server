e1dd7f6b5d6a9e11a456e3f82cd5672b0baca799
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2025-11-22 20:21:26 +0300

    feat: adds wss clients list

--- src/application.ts
+++ src/application.ts
@@ -1,14 +1,21 @@
-import {WebSocketServer} from 'ws';
+import {WebSocketServer, WebSocket} from 'ws';
 
 export class Application {
   constructor(
-    private readonly webSocketServer = new WebSocketServer({port: Number(process.env.PORT)})
+    private readonly webSocketServer = new WebSocketServer({port: Number(process.env.PORT)}),
+    private readonly clients: Set<WebSocket> = new Set()
   ) {}
 
   public startServer = () => {
     this.webSocketServer.on('connection', (webSocket) => {
+      this.clients.add(webSocket);
+
       webSocket.on('message', (data) => {
-        webSocket.send(data.toString('utf8'));
+        console.log(data.toString('utf8'));
+
+        for (const client of this.clients) {
+          client.send(data.toString('utf8'));
+        }
       });
     });
   };
